f81fb3b4db2e45e459377a0a3bd3669ae83e3e54
Emanuel Souza <emanuel.souza@postclick.com>
2021-11-02 17:24:01 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+const puppeteer = require('puppeteer');
+const https = require('https');
+const fs = require('fs');
+
+export class ImportImagesDto {
+  url: string;
+}
+
+@Controller()
+export class AppController {
+  @Post('importImage')
+  async importImages(@Body() importImagesDto: ImportImagesDto): Promise<void> {
+    try {
+      const browser = await puppeteer.launch();
+      const [page] = await browser.pages();
+
+      await page.goto(importImagesDto.url);
+
+      const imgURLs = await page.evaluate(() =>
+        Array.from(
+          document.querySelectorAll(
+            '[src*=".jpg"], [src*=".svg"], [src*=".gif"], [src*=".png"]',
+          ),
+          (elem: HTMLImageElement) => elem.src,
+        ),
+      );
+      console.log(imgURLs);
+      await browser.close();
+
+      imgURLs.forEach((imgURL, i) => {
+        https.get(imgURL, (response) => {
+          response.pipe(
+            fs.createWriteStream(`images/${i++}.${imgURL.slice(-3)}`),
+          );
+        });
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
